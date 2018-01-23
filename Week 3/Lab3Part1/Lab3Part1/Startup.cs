3110f2bc8faba3c5639b78c839b2b3ac4d1b1992
Button IV <wjbuttoniv@labs.neit.edu>
2018-01-22 19:37:32 -0500

    added week 3 work

---
+++ Week 3/Lab3Part1/Lab3Part1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab3Part1.Startup))]
+namespace Lab3Part1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
